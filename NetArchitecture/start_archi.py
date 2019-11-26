4723afca5e2d2bba040b2d8291ca34fee5455666
Jerome Bravais <jerome@macbook-4.local>
2019-11-26 09:47:35 +0100

    modif dockerfile

--- NetArchitecture/start_archi.py
+++ NetArchitecture/start_archi.py
@@ -6,23 +6,40 @@ from mininet.log import info, setLogLevel
 setLogLevel('info')
 
 net = Containernet(controller=Controller)
+
 info('*** Adding controller\n')
 net.addController('c0')
+
 info('*** Adding docker containers using ubuntu:trusty images\n')
-d1 = net.addDocker('d1', ip='10.0.0.251', dimage="ubuntu:trusty")
-d2 = net.addDocker('d2', ip='10.0.0.252', dimage="ubuntu:trusty")
+d1 = net.addDocker('d1', dimage="ubuntu:trusty")
+d2 = net.addDocker('d2', dimage="ubuntu:trusty")
+dc = net.addDatacenter("dc")
+
 info('*** Adding switches\n')
 s1 = net.addSwitch('s1')
 s2 = net.addSwitch('s2')
+
 info('*** Creating links\n')
 net.addLink(d1, s1)
-net.addLink(s1, s2, cls=TCLink, delay='100ms', bw=1)
+net.addLink(s1, s2)
 net.addLink(s2, d2)
+
+et.addLink(dc, s1)
+
+info('*** Starting RestApi\n')
+rapi1 = RestApiEndpoint("0.0.0.0", 5001)
+rapi1.connectDCNetwork(net)
+rapi1.connectDatacenter(dc)
+rapi1.start()
+
 info('*** Starting network\n')
 net.start()
+
 info('*** Testing connectivity\n')
 net.ping([d1, d2])
+
 info('*** Running CLI\n')
 CLI(net)
+
 info('*** Stopping network')
 net.stop()
