62b7f8ee563a83534b8c7c17830608ae5192ed6f
Jerome Bravais <jerome@macbook-4.local>
2019-12-03 11:06:14 +0100

    fix script containernet

--- NetArchitecture/start_archi.py
+++ NetArchitecture/start_archi.py
@@ -16,12 +16,10 @@ info('*** Adding docker containers using krustylebot/repo:sdci images\n')
 server = net.addDocker('server', ip='10.0.0.10', dimage=X)
 gwi = net.addDocker('gwi', ip='10.0.0.11', dimage=X)
 gwf = net.addDocker('gwf', ip='10.0.0.12', dimage=X)
-dc = net.addDatacenter("dc")
 
 info('*** Adding switches\n')
 s1 = net.addSwitch('s1')
 s2 = net.addSwitch('s2')
-s2 = net.addSwitch('s3')
 
 info('*** Creating links\n')
 net.addLink(server, s1)
@@ -29,12 +27,9 @@ net.addLink(s1, gwi)
 net.addLink(gwi, s2)
 net.addLink(s2, gwf)
 
-et.addLink(dc, s1)
-
 info('*** Starting RestApi\n')
 rapi1 = RestApiEndpoint("0.0.0.0", 5001)
 rapi1.connectDCNetwork(net)
-rapi1.connectDatacenter(dc)
 rapi1.start()
 
 info('*** Starting network\n')
