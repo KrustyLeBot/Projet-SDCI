9c9e17135360f7f854c1cb6bfb4b60ba80fbad67
KrustyLeBot <je.bravais@gmail.com>
2020-01-10 11:24:12 +0100

    ajout monitoring rest

---
+++ Monitoring/monitoring.js
@@ -0,0 +1,30 @@
+var express = require('express')
+var app = express()
+app.use(express.json()) // for parsing application/json
+var request = require('request');
+const si = require('systeminformation');
+
+
+function monitor(ip, port) {
+    console.log('ip : ' + ip );
+    console.log('port : ' + port );
+    const start = Date.now();
+    request("http://" + ip + ":" + port + "/ping");
+    return Date.now() - start;
+}
+
+
+app.get('/monitor/:ip/:port', function(req, res) {
+    var ip= req.params.ip;
+    var port = req.params.port;
+    var i;
+    var delay = 0;
+    for(i = 0; i<10; i++){
+    	delay += monitor(ip, port);
+    }
+    return delay/10.0
+});
+
+app.listen(8686 , function () {
+    console.log('listening on : ' + 8686 );
+});
