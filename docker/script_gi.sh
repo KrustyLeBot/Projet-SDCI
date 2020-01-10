6d9c56b11b83839e0908bfcdb531426a9f56725b
KrustyLeBot <je.bravais@gmail.com>
2020-01-10 11:44:34 +0100

    ajout monitoring rest

--- docker/script_gi.sh
+++ docker/script_gi.sh
@@ -2,4 +2,4 @@ rm gateway.js
 
 wget http://homepages.laas.fr/smedjiah/tmp/gateway.js
 
-node gateway.js --local_ip "${1}" --local_port 8080 --local_name "${3}" --remote_ip "${2}" --remote_port 8080 --remote_name "srv"
+node gateway.js --local_ip "${1}" --local_port 8080 --local_name "${3}" --remote_ip "${2}" --remote_port 8080 --remote_name "srv" &
--- docker/script_server.sh
+++ docker/script_server.sh
@@ -2,4 +2,4 @@ rm server.js
 
 wget http://homepages.laas.fr/smedjiah/tmp/server.js
 
-node server.js --local_ip "${1}" --local_port 8080 --local_name "srv"
+node server.js --local_ip "${1}" --local_port 8080 --local_name "srv" &
