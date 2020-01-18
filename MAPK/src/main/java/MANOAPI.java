7ba006bea7dc5d861623e6727123da2d9a408564
KrustyLeBot <je.bravais@gmail.com>
2020-01-18 14:43:25 +0100

    ajout deploeiment vnf mapk

--- MAPK/src/main/java/MANOAPI.java
+++ MAPK/src/main/java/MANOAPI.java
@@ -21,45 +21,38 @@ class MANOAPI {
         //printing
         for (Entry<String, String> e : vnfinfos.entrySet()) {
             Main.logger(this.getClass().getSimpleName(), "\t" + e.getKey() + " : " + e.getValue());
-        }
-        //TODO
-        URL url;
-        try {
-            url = new URL("http://127.0.0.1:5001/restapi/compute/cvim1/new_vnf");
-            HttpURLConnection httpCon = (HttpURLConnection) url.openConnection();
-            httpCon.setRequestMethod("PUT");
-            httpCon.setDoOutput(true);
-            httpCon.setRequestProperty("Content-Type", "application/json");
-
-            String payload = "{\"image\":\"krustylebot/repo:sdci_gw_virtualisee\"}";
-
-            OutputStreamWriter osw = new OutputStreamWriter(httpCon.getOutputStream());
-            osw.write(payload);
-            osw.flush();
-            osw.close();
             
-            BufferedReader in = new BufferedReader(new InputStreamReader(httpCon.getInputStream()));
-            String line;
-            while ((line = in.readLine()) != null) {
-                Pattern pattern = Pattern.compile("\"ip\": \"10.0.0.[0-9]\"");
-                Matcher matcher = pattern.matcher(line);
-                
-                if(matcher.find()){
-                    String[] split_list = line.split("\"",0);
-                    ip = split_list[split_list.length-1];
+            //TODO
+            URL url;
+            try {
+                Process process = Runtime.getRuntime().exec("vim-emu compute start -d dc1 -n " + (e.getValue()).get("name") + " -i krustylebot/repo:sdci_containernet");
+                StringBuilder output = new StringBuilder();
+
+                BufferedReader reader = new BufferedReader(
+                        new InputStreamReader(process.getInputStream()));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    Pattern pattern = Pattern.compile("\"ip\": \"10.0.0.[0-9]\"");
+                    Matcher matcher = pattern.matcher(line);
+                    
+                    if(matcher.find()){
+                        String[] split_list = line.split("\"",0);
+                        ip = split_list[split_list.length-1];
+                    }
                 }
+                in.close();
+                Main.logger(this.getClass().getSimpleName(), "IP GW VIRTUALISEE: " + ip);
+                
+            } catch (MalformedURLException e1) {
+                // TODO Auto-generated catch block
+                e1.printStackTrace();
+            } catch (IOException e1) {
+                // TODO Auto-generated catch block
+                e1.printStackTrace();
             }
-            in.close();
-            Main.logger(this.getClass().getSimpleName(), "IP GW VIRTUALISEE: " + ip);
-            
-        } catch (MalformedURLException e1) {
-            // TODO Auto-generated catch block
-            e1.printStackTrace();
-        } catch (IOException e1) {
-            // TODO Auto-generated catch block
-            e1.printStackTrace();
         }
-        
+
         return ip;
     }
 
